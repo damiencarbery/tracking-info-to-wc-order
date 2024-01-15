c8614fc2589d02e99c07953b8622af1ea22e6bc8
Damien Carbery <daymobrew@yahoo.com>
2024-01-15 13:37:50 +0000

    Update CMB2 object_types for HPOS; add dcwd_is_order_virtual_only() to determine whether order has shipping (and should have tracking); copy in dcwd_add_tracking_info_to_view_order_page() to add to tracking to View Order page; fix get_meta() and update_meta_data() calls to remove unnecessary $order_id.

--- tracking-info-to-wc-order.php
+++ tracking-info-to-wc-order.php
@@ -5,7 +5,7 @@ Plugin URI: https://www.damiencarbery.com/
 Description: Use CMB2 to add a custom metabox to add tracking information to WooCommerce orders. The information is then added to the "Completed Order" email. Also add custom REST API endpoint to receive info from Shippo.
 Author: Damien Carbery
 Author URI: https://www.damiencarbery.com
-Version: 0.3.20240112
+Version: 0.3.20240115
 */
 
 
@@ -20,10 +20,13 @@ add_action( 'before_woocommerce_init', function() {
 // Add the metabox to allow for manual entering (or editing) of tracking information.
 add_action( 'cmb2_admin_init', 'dcwd_order_metabox' );
 function dcwd_order_metabox() {
+	$woo_hpos_active = get_option( 'woocommerce_custom_orders_table_enabled' );
+	$object_types = ( 'yes' == $woo_hpos_active ) ? array( 'woocommerce_page_wc-orders' ) : array( 'shop_order' );
+
 	$cmb = new_cmb2_box( array(
 		'id'            => 'order_tracking_info',
 		'title'         => 'Tracking Information',
-		'object_types'  => array( 'shop_order', ), // Post type
+		'object_types'  => $object_types,
 		'context'       => 'side',
 		'priority'      => 'high',
 		'show_names'    => true, // Show field names on the left
@@ -38,6 +41,7 @@ function dcwd_order_metabox() {
 		'id'         => 'tracking_url',
 		'type'       => 'text_url',
 		'protocols'  => array( 'http', 'https' ),
+		'desc' => 'Be sure to add tracking data and click \'Update\' before setting the order status to \'Completed\', and clicking \'Update\' again. If not done in this order the email sent to the customer will not contain the tracking data.',
 	) );
 }
 
@@ -53,10 +57,43 @@ function dcwd_get_tracking_provider_from_url( $url ) {
 	if ( strpos( $url, 'ups.com' ) !== false ) {
 		return 'UPS';
 	}
+	if ( strpos( $url, 'dhl.' ) !== false ) {
+		return 'DHL';
+	}
 	// Add more as necessary.
+	if ( strpos( $url, 'www.singpost.com' ) !== false ) {
+		return 'Singapore Post';
+	}
 	
 	// Unknown provider.
 	return null;
+}
+
+
+// Determine whether an order has only virtual items (and therefore does not have shipping).
+function dcwd_is_order_virtual_only( $order_id ) {
+	$order = wc_get_order( $order_id );
+	$only_virtual = true;
+
+	if ( $order ) {
+		foreach ( $order->get_items() as $order_item ) {
+			$item = wc_get_product( $order_item->get_product_id() );
+			if ( !$item->is_type( 'virtual' ) && 'pw-gift-card' != $item->get_type() ) {
+				// This order contains a physical product so stop looking.
+				return false;
+			}
+		}
+
+		if ( $only_virtual ) {
+			//error_log( sprintf( '<p>Order: %d - Virtual.</p>', $order->get_id() ) );
+			return true;
+		}
+		else {
+			//error_log( sprintf( '<p>Order: %d - Contains physical products.</p>', $order->get_id() ) );
+		}
+	}
+
+	return false;
 }
 
 
@@ -70,8 +107,12 @@ function dcwd_add_tracking_info_to_order_completed_email( $order, $sent_to_admin
 */
 	if ( 'customer_completed_order' == $email->id ) {
 		$order_id = $order->get_id();
-		$tracking_number = $order->get_meta( $order_id, 'tracking_number', true );
-		$tracking_url = $order->get_meta( $order_id, 'tracking_url', true );
+
+		// Do not add tracking info if the order only has virtual items and does not have shipping.
+		if ( dcwd_is_order_virtual_only( $order_id ) ) { return; }
+
+		$tracking_number = $order->get_meta( 'tracking_number', true );
+		$tracking_url = $order->get_meta( 'tracking_url', true );
 		
 		// Quit if either tracking field is empty.
 		if ( empty( $tracking_number ) || empty( $tracking_url ) ) {
@@ -80,7 +121,7 @@ function dcwd_add_tracking_info_to_order_completed_email( $order, $sent_to_admin
 			echo '<h2>Tracking information</h2><p>Sorry, tracking information is not available at this time.</p>';
 			return;
 		}
-		
+
 		$tracking_provider = dcwd_get_tracking_provider_from_url( $tracking_url );
 
 		if ( $plain_text ) {
@@ -99,6 +140,35 @@ function dcwd_add_tracking_info_to_order_completed_email( $order, $sent_to_admin
 				printf( '<h2>Tracking information</h2><p>Your tracking number is <strong><a href="%s" style="color: #a7bf49">%s</a></strong>.</p>', esc_url( $tracking_url, array( 'http', 'https' ) ), esc_html( $tracking_number ) );
 			}
 		}
+	}
+}
+
+
+// Display tracking information in My Account area.
+add_action( 'woocommerce_view_order', 'dcwd_add_tracking_info_to_view_order_page', 5 );
+function dcwd_add_tracking_info_to_view_order_page( $order_id ) {
+	$order = wc_get_order( $order_id );
+	if ( ! $order ) { return; }
+	// Do not add tracking info if the order only has virtual items and does not have shipping.
+	if ( dcwd_is_order_virtual_only( $order_id ) ) { return; }
+
+	$tracking_number = $order->get_meta( 'tracking_number');
+	$tracking_url = $order->get_meta( 'tracking_url' );
+
+	// Quit if either tracking field is empty.
+	if ( empty( $tracking_number ) || empty( $tracking_url ) ) {
+		// Debugging code.
+		//error_log( sprintf( 'Order %d does not have both tracking number (%s) and url (%s)', $order_id, $tracking_number, $tracking_url ) );
+		echo '<p>Sorry, tracking information is not available at this time.</p>';
+		return;
+	}
+		
+	$tracking_provider = dcwd_get_tracking_provider_from_url( $tracking_url );
+	if ( ! empty( $tracking_provider ) ) {
+		printf( '<p>Your order has been shipped with <strong>%s</strong>. The tracking number is <strong><a href="%s">%s</a></strong>.</p>', $tracking_provider, esc_url( $tracking_url, array( 'http', 'https' ) ), esc_html( $tracking_number ) );
+	}
+	else {
+		printf( '<p>Your order has been shipped. The tracking number is <strong><a href="%s">%s</a></strong>.</p>', esc_url( $tracking_url, array( 'http', 'https' ) ), esc_html( $tracking_number ) );
 	}
 }
 
@@ -163,8 +233,8 @@ class Shippo_Hook_For_Tracking_Info extends WP_REST_Controller {
 
 				// Add the tracking info if both number and url pass sanitization checks.
 				if ( $tracking_number && $tracking_url ) {
-					$order->update_meta_data( $order_id, 'tracking_number', $tracking_number );
-					$order->update_meta_data( $order_id, 'tracking_url', $tracking_url );
+					$order->update_meta_data( 'tracking_number', $tracking_number );
+					$order->update_meta_data( 'tracking_url', $tracking_url );
 					
 					if ( $debug_mode ) {
 						$message = sprintf( 'Added tracking info: "%s" and "%s"', $tracking_number, $tracking_url );
